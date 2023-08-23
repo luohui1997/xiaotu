e3cda990e85aee57f4cc7e8309eb758d1386d5c9
luohui <2397447526@qq.com>
2023-08-23 11:23:07 +0800

    feat: 首页轮播图完善

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,10 +1,25 @@
 <script setup lang="ts">
+import { getHomeBanner } from '@/services/home'
 import CustomNavbar from './components/CustomNavbar.vue'
+import { onLoad } from '@dcloudio/uni-app'
+import { ref } from 'vue'
+import type { BannerItem } from '@/types/home'
+
+const bannerList = ref<BannerItem[]>([])
+
+const getHomeBannerData = async () => {
+  const res = await getHomeBanner()
+  bannerList.value = res.result
+}
+
+onLoad(() => {
+  getHomeBannerData()
+})
 </script>
 
 <template>
   <CustomNavbar />
-  <XtxSwiper />
+  <XtxSwiper :list="bannerList" />
 </template>
 
 <style lang="scss">
