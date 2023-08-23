70d4b8e439877f6782abb2fae37e5c29937da322
luohui <2397447526@qq.com>
2023-08-23 14:37:37 +0800

    feat: 分类组件

--- src/pages/index/index.vue
+++ src/pages/index/index.vue
@@ -1,9 +1,10 @@
 <script setup lang="ts">
-import { getHomeBanner } from '@/services/home'
-import CustomNavbar from './components/CustomNavbar.vue'
 import { onLoad } from '@dcloudio/uni-app'
 import { ref } from 'vue'
 import type { BannerItem } from '@/types/home'
+import { getHomeBanner } from '@/services/home'
+import CustomNavbar from './components/CustomNavbar.vue'
+import CategoryPanel from './components/CategoryPanel.vue'
 
 const bannerList = ref<BannerItem[]>([])
 
@@ -20,8 +21,11 @@ onLoad(() => {
 <template>
   <CustomNavbar />
   <XtxSwiper :list="bannerList" />
+  <CategoryPanel />
 </template>
 
 <style lang="scss">
-//
+page {
+  background-color: #f7f7f7;
+}
 </style>
